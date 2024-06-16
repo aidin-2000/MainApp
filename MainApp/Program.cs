d40a2a8d0c53ddef415c1c8be441bc093f303d39
Айдин Нурланбеков <nurlanbekovich.a@gmail.com>
2024-06-17 01:13:08 +0600

    Добавьте файлы проекта.

---
+++ MainApp/Program.cs
@@ -0,0 +1,7 @@
+﻿using Models;
+using Utilities;
+
+Person person = new Person("Aidin","Askerbekov");
+string fullName =PersonUtilities.GetFullName(person);
+Console.WriteLine($"Fullname: {fullName}");
+
